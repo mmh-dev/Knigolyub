eb47e4004346e00a3f9e5bc7ac51b21a46fe6795
IsakovAdi <adiletisakov94@mail.com>
2021-10-14 15:23:24 +0600

    Added layouts and fragments for students and admin

---
+++ app/src/main/java/com/mmh/knigolyub/admin/AdminBookConfigActivity.kt
@@ -0,0 +1,12 @@
+package com.mmh.knigolyub.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mmh.knigolyub.R
+
+class AdminBookConfigActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_book_config)
+    }
+}
